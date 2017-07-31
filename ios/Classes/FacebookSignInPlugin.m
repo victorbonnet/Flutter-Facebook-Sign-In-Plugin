e6936a2739da83996bc92ea4cc1ac7dcfdd44022
Frederick Cook <cook.frederick@gmail.com>
2017-07-31 02:01:26 -0600

    refactored ios plugin code to build and launch facebook login

--- ios/Classes/FacebookSignInPlugin.m
+++ ios/Classes/FacebookSignInPlugin.m
@@ -1,11 +1,8 @@
 #import "FacebookSignInPlugin.h"
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @implementation FacebookSignInPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFacebookSignInPlugin registerWithRegistrar:registrar];
-}
-
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options {
   return [[FBSDKApplicationDelegate sharedInstance] application:app
     openURL:url
@@ -21,58 +18,61 @@
     sourceApplication:sourceApplication
     annotation:annotation];
 }
-
-@implementation FacebookSignInPlugin {}
-
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
-    // controller needed to launch FBSDKLoginManager
-    FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+    
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     [[FBSDKApplicationDelegate sharedInstance] application:application
                              didFinishLaunchingWithOptions:launchOptions];
+    // Add any custom logic here.
+    //return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    return YES;
+}
+    
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
+    // controller needed to launch FBSDKLoginManager
+    FlutterViewController* controller = (FlutterViewController*)[[                                                                  [[UIApplication sharedApplication]delegate] window] rootViewController];
     
     FlutterMethodChannel *channel =
     [FlutterMethodChannel methodChannelWithName:@"facebook_sign_in"
                                 binaryMessenger:[registrar messenger]];
-    FirebaseAuthPlugin *instance = [[FirebaseAuthPlugin alloc] init];
-    [registrar addMethodCallDelegate:instance channel:channel];
-}
-
-- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
-    if ([@"login" isEqualToString:call.method]) {
-        FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
-        [login
-         logInWithReadPermissions: @[@"public_profile", @"email"]
-         fromViewController:controller
-         handler:^(FBSDKLoginManagerLoginResult *fbResult, NSError *error) {
-             if (error) {
-                 result(@"Process error");
-             } else if (fbResult.isCancelled) {
-                 result(@"Cancelled");
-             } else {
-                 result(@"Logged in");
-             }
-         }];
-    } else if ([@"checkFacebookLogin" isEqualToString:call.method]) {
-        if ([FBSDKAccessToken currentAccessToken]) {
-            FBSDKAccessToken *accessToken = [FBSDKAccessToken currentAccessToken];
-            NSLog(@"Logged in:");
-            NSLog(@"%@", accessToken);
-            result(@"YES");
+    
+    [channel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        if ([@"login" isEqualToString:call.method]) {
+            FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+            [login
+             logInWithReadPermissions: @[@"public_profile", @"email"]
+             fromViewController:controller
+             handler:^(FBSDKLoginManagerLoginResult *fbResult, NSError *error) {
+                 if (error) {
+                     result(@"Process error");
+                 } else if (fbResult.isCancelled) {
+                     result(@"Cancelled");
+                 } else {
+                     result(@"Logged in");
+                 }
+             }];
+        } else if ([@"checkFacebookLogin" isEqualToString:call.method]) {
+            if ([FBSDKAccessToken currentAccessToken]) {
+                FBSDKAccessToken *accessToken = [FBSDKAccessToken currentAccessToken];
+                NSLog(@"Logged in:");
+                NSLog(@"%@", accessToken);
+                result(@"YES");
+            } else {
+                NSLog(@"not logged in");
+                result(@"NO");
+            }
+        } else if ([@"getFacebookToken" isEqualToString:call.method]) {
+            FBSDKAccessToken *token = [FBSDKAccessToken currentAccessToken];
+            if (token) {
+                result(@{@"tokenString" : token.tokenString});
+            } else {
+                result(nil);
+            }
         } else {
-            NSLog(@"not logged in");
-            result(@"NO");
+            result(FlutterMethodNotImplemented);
         }
-    } else if ([@"getFacebookToken" isEqualToString:call.method]) {
-        FBSDKAccessToken *token = [FBSDKAccessToken currentAccessToken];
-        if (token) {
-            result(@{@"tokenString" : token.tokenString});
-        } else {
-            result(nil);
-        }
-    } else {
-        result(FlutterMethodNotImplemented);
-    }
+    }];
 }
 
 @end
